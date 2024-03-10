af3f0a5b6789982ceb2e3c95848d204db527280f
idkup <idkup137@gmail.com>
2024-03-10 12:26:45 -0400

    ???

--- src/views/BoxView.vue
+++ src/views/BoxView.vue
@@ -31,10 +31,10 @@
 			async loadBox() {
 				this.loadingBox = true;
 				const resp = await fetch("https://rebornwebserver.pages.dev/box/");
-				let mon_arr: number[];
+				let mon_arr: string[] = [];
 				const mons = await resp.json();
 				for (let mon in mons) {
-					mon_arr.push(mon.species);
+					mon_arr.push(mon);
 				}
 				this.mons = mon_arr;
 				this.loadingBox = false;
